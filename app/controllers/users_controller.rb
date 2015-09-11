028bb870cc3daf9df499b1adf1a1b6c41b1423d2
Ryosuke Hiura <RyosukeHiura@Ryosuke-MacBook-Pro.local>
2015-09-11 17:57:29 +0900

    ユーザー認証

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:index, :edit, :update]
+	before_action :correct_user, only: [:edit, :update]
+
+	def index
+		@users = User.all
+	end
+	
 	def new
 	  @user = User.new
 	end
  
 	def show
 	  @user = User.find(params[:id])
+	end
+	
+	def edit
+	end
+	
+	def update
+		if @user.update_attributes(user_params)
+		 	flash[:success] = "Profile updated"
+		 	redirect_to @user
+		 else
+		 	render 'edit'
+		 end
 	end
 
 	def create
@@ -17,10 +36,23 @@ class UsersController < ApplicationController
 			render 'new'
 		end	
 	end
-
+end
 
 private
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-		end
 	end
+	
+	# Before actions
+	
+	def signed_in_user
+		unless signed_in?
+		store_location
+		redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	end
+	
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
+end
